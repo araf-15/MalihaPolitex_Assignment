fd874b919fd3d8f1b6b6ec392ede8ed10a3ebf35
araf-15 <hasan.araf15@gmail.com>
2021-05-03 17:02:59 +0600

    Integrated Admin LTE With the project

---
+++ Task_1/Task_1/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_1.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Enroll()
+        {
+            return View();
+        }
+    }
+}
---
+++ Task_1/Task_1/Areas/Admin/Models/CourseModel.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using System.ComponentModel.DataAnnotations;
+using Task_1.Foundation.Entities;
+using Task_1.Foundation.Services;
+
+namespace Task_1.Areas.Admin.Models
+{
+    public class CourseModel
+    {
+        private readonly ICourseService _courseService;
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int SeatCout { get; set; }
+        [Required]
+        public int Fee { get; set; }
+
+        public CourseModel()
+        {
+            _courseService = Startup.AutofacContainer.Resolve<ICourseService>();
+        }
+
+        public CourseModel(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public void Add()
+        {
+            _courseService.AddCourse(new Course
+            {
+                Title = Title,
+                SeatCount = SeatCout,
+                Fee = Fee
+            });
+        }
+    }
+}
